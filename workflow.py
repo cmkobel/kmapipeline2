a6feeeea7c74a46915d9b15a2853bd77b067f933
Carl M. Kobel <kobel@protonmail.ch>
2020-06-20 10:32:34 +0200

    amend

--- workflow.py
+++ workflow.py
@@ -197,13 +197,15 @@ for prefix, path in reads_paths_parsed.items():
             inputs = [f"output/isolates/{full_name}/trim_reads/PE_R1_val_1.fq.gz",
                       f"output/isolates/{full_name}/trim_reads/PE_R2_val_2.fq.gz"],
             outputs = [f"output/isolates/{full_name}/kraken2/kraken2_reads_report.txt",
-                       f"output/isolates/{full_name}/kraken2/kraken2_reads_top10.tab"],
+                       f"output/isolates/{full_name}/kraken2/kraken2_reads_top10.tab_again"],
             cores = 8,
             memory = '8gb',
             account = 'clinicalmicrobio') << f"""
 
                 mkdir -p output/isolates/{full_name}/kraken2
-                kraken2 --db /project/ClinicalMicrobio/faststorage/database/minikraken_8GB_20200312/ --report output/isolates/{full_name}/kraken2/kraken2_reads_report.txt --paired output/isolates/{full_name}/trim_reads/PE_R1_val_1.fq.gz output/isolates/{full_name}/trim_reads/PE_R2_val_2.fq.gz > /dev/null
+                
+                kraken2 --db /project/ClinicalMicrobio/faststorage/database/minikraken2_v2_8GB_201904_UPDATE/ --report output/isolates/{full_name}/kraken2/kraken2_reads_report_old.txt --paired output/isolates/{full_name}/trim_reads/PE_R1_val_1.fq.gz output/isolates/{full_name}/trim_reads/PE_R2_val_2.fq.gz > /dev/null
+                #kraken2 --db /project/ClinicalMicrobio/faststorage/database/minikraken_8GB_20200312/ --report output/isolates/{full_name}/kraken2/kraken2_reads_report.txt --paired output/isolates/{full_name}/trim_reads/PE_R1_val_1.fq.gz output/isolates/{full_name}/trim_reads/PE_R2_val_2.fq.gz > /dev/null
 
                 cat output/isolates/{full_name}/kraken2/kraken2_reads_report.txt | {kraken_reads_top_command} | sort -gr | head -n 10 > output/isolates/{full_name}/kraken2/kraken2_reads_top10.tab
 
@@ -215,7 +217,7 @@ for prefix, path in reads_paths_parsed.items():
                       f"output/isolates/{full_name}/trim_reads/PE_R2_val_2.fq.gz"],
             outputs = [],
             cores = 8,
-            memory = '16gb',
+            memory = '128gb',
             walltime = '2-00:00:00',
             account = 'clinicalmicrobio') << f"""
 
